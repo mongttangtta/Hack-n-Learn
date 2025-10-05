3d8e02db018fe190c0404ae011e32870a6726b33
JUNHOCHOI0309 <skyho0309@naver.com>
2025-10-06 02:16:31 +0900

    feat : 댓글 모델 추가

---
+++ backend/src/models/comment.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const communitySchema = new mongoose.Schema(
+        {
+                postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+                parentComment: { type: mongoose.Schema.Types.ObjectId, ref: "Comment", default: null },
+                content: { type: String, required: true, trim: true },
+                author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+        },
+        { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", communitySchema);
+
+export default Comment;
