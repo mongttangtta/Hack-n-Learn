7acf1c06a48f19f04eff5af60cb9705f9474ad45
dlakrp123 <dlakrp731@gmail.com>
2025-10-20 01:22:38 +0900

    Feat: 계정 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import AIChatBot from './components/AIChatBot';
+import LoginPage from './pages/login';
 import SignupPage from './pages/signup';
 
 export default function App() {
   return (
     <>
-      <SignupPage />
+      <LoginPage />
     </>
   );
 }
