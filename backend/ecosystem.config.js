5cf15d88d552909fb5d160c5f03a213a5d0d10d6
JUNHOCHOI0309 <skyho0309@naver.com>
2025-10-25 13:57:04 +0900

    chore : pm2 설정파일 수정

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,20 +1,18 @@
-module.exports = {
-  apps: [
-    {
-      name: "hacknlearn-server",
-      script: "./server.js",  // 또는 "./app.js" (실제 메인 파일명에 맞춰 수정)
-      exec_mode: "cluster",
-      instances: 1,
-      env: {
-        NODE_ENV: "production",
-        PORT: 3000
-      }
-    },
-    {
-      name: "practice-cleaner",
-      script: "./src/cron/cleanup.js",
-      cron_restart: "*/5 * * * *",
-      autorestart: true
+export const apps = [
+  {
+    name: "hacknlearn-server",
+    script: "./server.js",
+    exec_mode: "cluster",
+    instances: 1,
+    env: {
+      NODE_ENV: "production",
+      PORT: 3000
     }
-  ]
-};
+  },
+  {
+    name: "practice-cleaner",
+    script: "./cron/cleanup.js",
+    cron_restart: "*/5 * * * *",
+    autorestart: true
+  }
+];
