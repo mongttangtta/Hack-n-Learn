6169d742b72d784775f86ecff618a6cb53caed79
JUNHOCHOI0309 <skyho0309@naver.com>
2025-11-20 15:58:39 +0900

    fix : qeury -> body

--- backend/src/controllers/chatBot.controller.js
+++ backend/src/controllers/chatBot.controller.js
@@ -48,7 +48,7 @@ export class ChatBotController {
                 try {
                         const userId = req.session?.userId || null;
                         const guestThreadId = req.cookies?.guestThreadId;
-                        const { threadId } = req.query || {};
+                        const threadId = req.body.threadId;
                         const limit = Number(req.query.limit) || 50;
 
                         if(userId){
