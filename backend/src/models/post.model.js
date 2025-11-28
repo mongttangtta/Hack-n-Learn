54d4086e578a6e78709017f41ad7653bbd24735a
JUNHOCHOI0309 <skyho0309@naver.com>
2025-11-29 01:02:19 +0900

    fix : 게시글 nickname required 도 제거

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -6,7 +6,7 @@ const PostSchema = new mongoose.Schema(
                 title: { type: String, required: true , trim: true },
                 content: { type: String, required: true },
                 author : { type : mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-                nickname : { type : String, required: true },
+                nickname : { type : String },
                 views: { type: Number, default: 0 },
         },
         { timestamps: true }
