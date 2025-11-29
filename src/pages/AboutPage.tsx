a008e5e6ba3d45e9a83c71149ef9f5fcf3e90053
dlakrp123 <dlakrp731@gmail.com>
2025-11-29 19:37:26 +0900

    Feat: 서비스소개 페이지 퍼블리싱

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import Stepper, { Step } from '@/components/Stepper';
 
-const AboutPage: React.FC = () => {
+export default function AboutPage() {
   return (
-    <div className="min-h-screen bg-gray-900 text-neutral-200 flex items-center justify-center">
-      <h1 className="text-4xl font-bold">서비스 소개 Page</h1>
-    </div>
+    <>
+      <Stepper
+        initialStep={1}
+        onStepChange={(step) => {
+          console.log(step);
+        }}
+        onFinalStepCompleted={() => console.log('All steps completed!')}
+        backButtonText="Previous"
+        nextButtonText="Next"
+      >
+        <Step>
+          <h2>Welcome to the React Bits stepper!</h2>
+          <p>Check out the next step!</p>
+        </Step>
+        <Step>
+          <h2>Step 2</h2>
+          <img
+            style={{
+              height: '100px',
+              width: '100%',
+              objectFit: 'cover',
+              objectPosition: 'center -70px',
+              borderRadius: '15px',
+              marginTop: '1em',
+            }}
+            src="https://www.purrfectcatgifts.co.uk/cdn/shop/collections/Funny_Cat_Cards_640x640.png?v=1663150894"
+          />
+          <p>Custom step content!</p>
+        </Step>
+        <Step>
+          <h2>How about an input?</h2>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Your name?"
+          />
+        </Step>
+        <Step>
+          <h2>Final Step</h2>
+          <p>You made it!</p>
+        </Step>
+      </Stepper>
+    </>
   );
-};
-
-export default AboutPage;
+}
